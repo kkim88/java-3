449b7eeab3894f1bf1494fbf0ddedda4c88fc2a8
Katherine Kim <katherinekim88@gmail.com>
2020-07-05 20:22:56 -0500

    answers

--- src/regex/Regex.java
+++ src/regex/Regex.java
@@ -1,13 +1,18 @@
 package regex;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class Regex {
     public static void main(String[] args) {
 
         // 1. What does the following pattern match? (\d){36} explain in a println() statement.
 
-        System.out.println("The following pattern ('\\d'){36} matches : ...");
+        System.out.println("The following pattern ('\\d'){36} matches : any 36 digits in a row");
 
 
 
@@ -16,26 +21,56 @@ class Regex {
         // 2. Create a new array of the first names of the TEKmentors.  Use Regex to only grab the first name of every TEKmentor.  Push the values to a new array
         String[] tekMentors;
 
+        Pattern p = Pattern.compile("(Amir|Mark|Rosa|Desaree|Abram|Dylan|Demilios|Jonathan)");
+        List<String> matches = new ArrayList<String>();
+        for (int i = 0; i < TEKmentors.length; i++) {
+            if (TEKmentors[i].matches("(Amir|Mark|Rosa|Desaree|Abram|Dylan|Demilios|Jonathan)")) { // matches uses regex
+                matches.add(TEKmentors[i]);
+            }
+        }
+            System.out.println(matches);
+
+
 
         // 3. Find all the occurences of any form of 'book' in the following paragraph. use regex to match the occurences and store the count of books in an int.
         
         int bookCount = 0;
         String bookText = "Books are the keys to knowledge.  I didn't like to read books as a child, but in college I started enjoying learning and reading books. You can borrows books from the library, or you can buy them from the bookstore. I'm not sure if I prefer paperback books or hardcover books.  I'm such a nerd that I even like textbooks.  With the advent of technology, you can even buy digital books, such as kindle-books, nook-books, or other e-books. My personal favorite book format are pdf-books, because I don't have to carry so many books around wherever I go.  All the books are on my ipad or laptop.  When I lived abroad, they would give books to students absolutely free.  Free books for a student of knowledge is like a kid in a candy store.  So wipe the dust off of your books, and remember the slogan from 'reading rainbow' : 'Take a look! It's in a book! Reading Rainbow!";
+        Pattern pattern = Pattern.compile("book", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(bookText);
+        while (matcher.find())
+            bookCount++;
+        System.out.println(bookCount);
         System.out.println(bookText);
 
-        // 4.a Create an array of all the words besides the word 'sleepy'.  Each word does not have to be a separate element, although you can split it that way if you wish.  We just want an array that everything that is not 'sleepy'.  
+        // 4.a Create an array of all the words besides the word 'sleepy'.  Each word does not have to be a separate element, although you can split it that way if you wish.  We just want an array that everything that is not 'sleepy'.
+
+        // ^((?!sleepy).)*$
 
         String sleepy = "I felt sleepy because I saw the others were sleepy and because I knew I should feel sleepy, but I wasn't really sleepy.  If you're sleepy and you know it, clap your hands.  Keep on being sleepy until you actually become sleepy";
 
+        String[] noSleepy = Pattern.compile("^((?!sleepy).)*$")
+                .matcher(sleepy)
+                .results()
+                .map(MatchResult::group)
+                .toArray(String[]::new);
+
         // 4.b combine the array that you just created into a string
-        String not_sleepy; //punctuation marks will be here
+
+        String not_sleepy = Arrays.toString(noSleepy); //punctuation marks will be here
+
+        System.out.println(not_sleepy);
 
         //4.c remove the punctuation marks from the notSleepy string.
-        String notSleepy; //no punctuation marks should be here.
+        String notSleepy = not_sleepy; //no punctuation marks should be here.
+        notSleepy.replaceAll("[^a-zA-Z ]", "");
+        System.out.println(notSleepy);
 
         //4.d Now replace all the occurences of 'sleepy' with the word 'happy'.  Call the new string happy.  
 
-        String happy;
+        String happy = sleepy;
+        happy = happy.replace("sleepy", "happy");
+        System.out.println(happy);
 
         //BONUS : 
         //5. You are looking for unicode arrow symbols in a string.  https://jrgraphix.net/r/Unicode/2190-21FF is a selection of unicode arrow symbols to aid you in your search.  Match all the codes that are arrows, and then print them out to the console.  They should be printing out as the arrow images.
